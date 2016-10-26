68d6d98ac8b51689cc73e3c23fef35138c2bc08e
MrXlVii <nale2892@gmail.com>
2016-10-25 20:59:14 -0400

    changed XOR function

--- many-time-pad.py
+++ many-time-pad.py
@@ -28,10 +28,10 @@ def main():
 
   cipherTen = "466d06ece998b7a2fb1d464fed2ced7641ddaa3cc31c9941cf110abbf409ed39598005b3399ccfafb61d0315fca0a314be138a9f32503bedac8067f03adbf3575c3b8edc9ba7f537530541ab0f9f3cd04ff50d66f1d559ba520e89a2cb2a83"
 
-'''
-Target: 
-32510ba9babebbbefd001547a810e67149caee11d945cd7fc81a05e9f85aac650e9052ba6a8cd8257bf14d13e6f0a803b54fde9e77472dbff89d71b57bddef121336cb85ccb8f3315f4b52e301d16e9f52f904
-'''
+  Target = "32510ba9babebbbefd001547a810e67149caee11d945cd7fc81a05e9f85aac650e9052ba6a8cd8257bf14d13e6f0a803b54fde9e77472dbff89d71b57bddef121336cb85ccb8f3315f4b52e301d16e9f52f904"
+
+
+  
 
 # Bitwise XOR is ^ in python
 
@@ -43,11 +43,10 @@ Target:
 
 def XOR(A, B):
   #Takes in two ciphers and XORs them, then reveals the first cipher's message.
-  
-    temp1 = A ^ B
-    temp2 = temp1 ^ B
-  
-    return temp2
+  bitA = A.decode("hex") 
+  bitB = B.decode("hex")
+
+  return "".join(chr(ord(x) ^ ord(y)) for x, y in zip(bitA, bitB)
   
   
 
