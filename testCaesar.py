b149d3e2be52a9876815b4599164210f086cf0c0
MrXlVii <nale2892@gmail.com>
2016-11-08 23:06:45 -0500

    update TODO objectives

--- testCaesar.py
+++ testCaesar.py
@@ -4,8 +4,8 @@ import Caesar
 class TestCryptMethods(unittest.TestCase):
     """Tests for Caesar.py"""
     cryptInput = ['encrypt', 'Encrypt', 'decrypt', 'Decrypt', 'blah', 'WHOCARES']
-    encryptInput = ['foo', 'bar', 'Hello World', '342', '101010111']
-    decryptInput = ['ktt', 'gfw', 'Mjqqt%\twqi', '897', '656565666']
+    encryptInput = ['foo', 'bar', 'Hello World', 'xyz', '101010111']
+    decryptInput = ['ktt', 'gfw', 'Mjqqt Btwqi', 'cde', '101010111']
     
     def setUp(self):
         pass
@@ -23,12 +23,16 @@ class TestCryptMethods(unittest.TestCase):
         
     def test_encryption(self):
         for i in range(len(encryptInput)):
-            result[i] = Caesar.crypt(encryptInput[i])
+            result.append(Caesar.encryption(self.encryptInput[i]))
+    
+    #TODO: test encryption runs appropriately
   
         
         
     def test_decryption(self):
         pass
+    
+    #TODO: test decryption runs appropriately
 
 if __name__ == "__main__":
     unittest.main()    
