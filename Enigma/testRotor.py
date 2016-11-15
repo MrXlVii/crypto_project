72d2ff2555290d55ea0679afb143744af570740e
MrXlVii <nale2892@gmail.com>
2016-11-14 22:56:19 -0500

    created rotor unit test skeleton code

---
+++ Enigma/testRotor.py
@@ -0,0 +1,25 @@
+
+import unittest
+import Rotor.py
+
+
+class testRotor(unittest.TestCase):
+     #test for Rotor.py
+     
+     testConfig = []
+     
+     def test_Instance(self, config):
+         pass
+     
+     def test_Rotate(self):
+         pass
+     
+     def test_Set_Start(self,postion):
+         pass
+     
+     def test_Display_Position(self):
+         pass
+         
+if __name__ == "__main__":
+    unittest.main()
+         
