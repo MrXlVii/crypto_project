5f8914a21925776d2d6eb5cfef104bfe9df399a1
MrXlVii <nale2892@gmail.com>
2016-11-15 23:03:23 -0500

    created Core skeleton

---
+++ Enigma/Core.py
@@ -0,0 +1,22 @@
+
+
+def class Core:
+    """Logic core for Enigma Machine, corresponding to the circuitry"""
+    
+    def buildRotors():
+        #rotor configuration/build
+        pass
+        
+    def arrangeRotors():
+        #arranges the order of each individual rotor
+        pass
+    
+    def encrypt():
+        #performs the encryption logic
+        pass
+        
+    def decrypt():
+        #performs teh decryption logic
+        pass
+    
+    
