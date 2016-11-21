89a905c9ec354b9ab2ea8ed0a7c5bff8aa94e19c
MrXlVii <nale2892@gmail.com>
2016-11-21 18:46:39 -0500

    created testCore.py unittest

---
+++ Enigma/testCore.py
@@ -0,0 +1,25 @@
+"""
+
+This is the test for Core.py
+
+"""
+
+import unittest
+from Core import Core
+
+def class testCore:
+    
+    def test_Build_Rotors(self):
+        pass
+    
+    def test_Arrange_Rotors(self):
+        pass
+        
+    def test_Encrypt(self):
+        pass
+        
+    def test_Decrypt(self):
+        pass
+    
+if __name__ == "__main__":
+    unittest.main()
