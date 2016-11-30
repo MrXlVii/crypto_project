72848c8af06a35a0d32da6ce4554bb235042d484
MrXlVii <nale2892@gmail.com>
2016-11-29 21:52:23 -0500

    created machine unittest skeleton

---
+++ Enigma/tests/testMachine.py
@@ -0,0 +1,30 @@
+import unittest
+from enigma import machine
+
+class TestRotorMethods(unittest.TestCase):
+    #Test for Rotor object
+    
+    def test_Init(self):
+        pass
+    
+    def test_Rotate(self):
+        pass
+    
+    def test_SetStart(self):
+        pass
+
+class TestCoreMethods(unittest.TestCase):
+    #Test for Core object
+    
+    def test_Config(self):
+        pass
+    
+    def test_Encrypt(self):
+        pass
+    
+    def test_Decrypt(self):
+        pass
+    
+
+if "__name__" == "__main__":
+    unittest.main()
