e63e4493750207b88f982a369abf0e0f26798431
MrXlVii <nale2892@gmail.com>
2016-11-30 22:08:52 -0500

    updated testMachine

--- Enigma/tests/testMachine.py
+++ Enigma/tests/testMachine.py
@@ -3,15 +3,67 @@ from enigma import machine
 
 class TestRotorMethods(unittest.TestCase):
     #Test for Rotor object
+    c1= ['A', 'B', 'C']
+    c2= ['D', 'E', 'F']
+    c3= ['G']
+      
     
     def test_Init(self):
-        pass
+        R1 = Rotor(self.c1)
+        self.assertEqual(R1.rotorNum, 1)
+        R2 = Rotor(self.c2)
+        self.assertEqual(R2.rotorNum, 2)
+        R3 = Rotor(self.c3)
+        self.assertEqual(R2.rotorNum, 3)
+         
+        for i in range(4,100):
+           R = Rotor(self.c1)
+             
+        self.assertEqual(R.rotorNum, 99)
+         
+        print R1.config
+        self.assertEqual(R1.config, self.c1, 'failed to install correct rotor config')
+        print R2.config
+        self.assertEqual(R2.config, self.c2, 'failed to install correct rotor config')
+        print R3.config
+        self.assertEqual(R3.config, self.c3, 'failed to install correct rotor config')
     
     def test_Rotate(self):
-        pass
+        R1 = Rotor(self.c1)
+        R2 = Rotor(self.c2)
+        R3 = Rotor(self.c3)
+             
+        R1.setStart(0)
+        R2.setStart(0)
+        R3.setStart(0)
+         
+        R1.rotate()
+        self.assertEqual(R1.POSITION, 1)
+         
+        for i in range(0,100):
+            R2.rotate()
+        self.assertEqual(R2.POSITION, 100)
+        
+        #TODO: Determine how we'd like to handle modulo positions 
+        R3.rotate()
+        self.assertEqual(R3.POSITION, 0, 'should not rotate, there is only one value')
     
     def test_SetStart(self):
-        pass
+        R1 = Rotor(self.c1)
+        R2 = Rotor(self.c2)
+        R3 = Rotor(self.c3) 
+         
+        P1 = 0
+        P2 = 1
+        P3 = 35
+         
+        R1.setStart(P1)
+        R2.setStart(P2)
+        R3.setStart(P3)
+         
+        self.assertEqual(R1.POSITION, P1, 'incorrect position')
+        self.assertEqual(R2.POSITION, P2, 'incorrect position')
+        self.assertEqual(R3.POSITION, P3, 'does not account for modulo--optional')
 
 class TestCoreMethods(unittest.TestCase):
     #Test for Core object
@@ -28,3 +80,4 @@ class TestCoreMethods(unittest.TestCase):
 
 if "__name__" == "__main__":
     unittest.main()
+
