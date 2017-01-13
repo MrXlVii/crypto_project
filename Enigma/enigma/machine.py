eb0aa22262ea0a3eb7aeeddfd38655dd63652013
MrXlVii <nale2892@gmail.com>
2017-01-13 00:47:08 -0500

    updated TODO/comments

--- Enigma/enigma/machine.py
+++ Enigma/enigma/machine.py
@@ -64,7 +64,7 @@ class Core(object):
         self.r3.POSITION = p3
         
     def encrypt(self, plain):
-
+        #TODO: Determine what to do with lowercase
         cipher = list(plain)
 
         for i in range(len(cipher)):
@@ -106,7 +106,7 @@ class Core(object):
         return output
         
     def decrypt(self, cipher):
-        
+        #TODO: Determine what to do with lowercase
         plain = list(cipher)
         
         for i in range(len(plain)):
@@ -163,9 +163,9 @@ class Machine:
         frame = Frame(master)
         frame.pack()
 
-        self.R1 = Rotor(['E', 'K', 'M', 'F', 'L', 'G', 'D', 'Q', 'V', 'Z', 'N', 'T', 'O', 'W', 'Y', 'H', 'X', 'U', 'S', 'P', 'A', 'I', 'B', 'R', 'C', 'J'])
-        self.R2 = Rotor(['A', 'J', 'D', 'K', 'S', 'I', 'R', 'U', 'X', 'B', 'L', 'H', 'W', 'T', 'M', 'C', 'Q', 'G', 'Z', 'N', 'P', 'Y', 'F', 'V', 'O', 'E'])
-        self.R3 = Rotor(['B', 'D', 'F', 'H', 'J', 'L', 'C', 'P', 'R', 'T', 'X', 'V', 'Z', 'N', 'Y', 'E', 'I', 'W', 'G', 'A', 'K', 'M', 'U', 'S', 'Q', 'O'])        
+        R1 = Rotor(['E', 'K', 'M', 'F', 'L', 'G', 'D', 'Q', 'V', 'Z', 'N', 'T', 'O', 'W', 'Y', 'H', 'X', 'U', 'S', 'P', 'A', 'I', 'B', 'R', 'C', 'J'])
+        R2 = Rotor(['A', 'J', 'D', 'K', 'S', 'I', 'R', 'U', 'X', 'B', 'L', 'H', 'W', 'T', 'M', 'C', 'Q', 'G', 'Z', 'N', 'P', 'Y', 'F', 'V', 'O', 'E'])
+        R3 = Rotor(['B', 'D', 'F', 'H', 'J', 'L', 'C', 'P', 'R', 'T', 'X', 'V', 'Z', 'N', 'Y', 'E', 'I', 'W', 'G', 'A', 'K', 'M', 'U', 'S', 'Q', 'O'])        
 
         q1 = Label(text = 'Which rotor configuration are you using?')
         q1.pack()
@@ -178,7 +178,7 @@ class Machine:
         listbox.bind("<Double-Button-1>") #determine what to bind to
 
         rotorCon = { 0: Core(R1, R2, R3), 1: Core(R1, R3, R2), 2: Core(R2, R1, R3), 3: Core(R2, R3, R1), 4: Core(R3, R1, R2), 5: Core(R3, R2, R1)}
-        core = rotorCon[#selection from button]
+        core = rotorCon[listbox.curselection()] #needs to be changed
 
         q2 = Label(text = 'Which position will each rotor be set to?')
         q2.pack()
@@ -199,11 +199,15 @@ class Machine:
         lb2.bind()
         lb3.bind()
         
-        #TODO: Button that records the selections above
+        p1 = lb1.curselection()
+        p2 = lb2.curselection()
+        p3 = lb3.curselection()
+
         confirm = Button(text = 'Confirm', command = lambda: messageInput(core, p1, p2, p3))
         confirm.pack()
 
     def messageInput(self, core, p1, p2, p3):
+        #TODO: kill previous frame, reveal only entry box
 
         greeting = Label(text = 'Would you like to encrypt or decrypt this message?')
         greeting.pack(side = TOP)
@@ -217,12 +221,10 @@ class Machine:
         self.deBut.pack()
 
     def enBut(self, core, plain):
-        #TODO: determine how to get input for this method
         cipher = core.encrypt(plain)
         output = Label(text = cipher)
         
     def deBut(self, core, cipher):
-        #TODO: determine how to get input for this method
         plain = core.decrypt(cipher)
         output = Label(text = plain)
 
