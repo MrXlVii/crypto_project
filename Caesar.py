165fd261035c13fabf02fb53b719f0209fe52ab6
MrXlVii <nale2892@gmail.com>
2017-02-08 00:59:16 -0500

    updated old files to PEP8 styling

--- Caesar.py
+++ Caesar.py
@@ -7,134 +7,142 @@ Enter the key number (1-26)  13
 Your translated text is:  Gur fxl nobir gur cbeg jnf gur pbybe bs gryrivfvba, gharq gb n qrnq punaary.
 """
 
+
 def main():
-	
-	while(True):
-		en_de_crypt = raw_input('Do you wish to "encrypt" or "decrypt" a message? ')
-		if (en_de_crypt == 'encrypt') or (en_de_crypt =='Encrypt') or (en_de_crypt =='decrypt') or (en_de_crypt =='Decrypt'):
-		    break
-		else:
-			print "you need to write 'encrypt' or 'decrypt'"
-			continue
 
-	while(True):
-		inputMessage = raw_input('Enter your message: ')
-		if inputMessage is None:
-			print "you need to enter a message"
-			continue
-		else:
-			break
-	
-	while(True):
-		keySize = raw_input('Enter the key number (1-26): ')
-		keySize = int(keySize)
-		if keySize is None:
-			print "you need to enter a key number"
-			continue
-		elif type(keySize) is int:
-			if keySize > 26 or keySize < 1:
-				print "enter a value between 1 and 26"
-				continue
-			else:
-			    logic = crypt(en_de_crypt)
-			    break
+    while(True):
+        en_de_crypt = raw_input('Do you wish to "encrypt" or "decrypt" a message? ')
+        if{
+           (en_de_crypt == 'encrypt') or
+           (en_de_crypt == 'Encrypt') or
+           (en_de_crypt == 'decrypt') or
+           (en_de_crypt == 'Decrypt')
+           }:
+            break
+        else:
+            print "you need to write 'encrypt' or 'decrypt'"
+            continue
 
-		else:
-			print "Enter an integer key number (1-26)"
-			pass
-			
-	if logic is True:
-	    cipher= encryption(inputMessage, keySize)
-	    print cipher
-	elif logic is False:
-	    plain= decryption(inputMessage, keySize)
-	    print plain
-	else:
-	    pass
-              
+    while(True):
+        inputMessage = raw_input('Enter your message: ')
+        if inputMessage is None:
+            print "you need to enter a message"
+            continue
+        else:
+            break
+
+    while(True):
+        keySize = raw_input('Enter the key number (1-26): ')
+        keySize = int(keySize)
+        if keySize is None:
+            print "you need to enter a key number"
+            continue
+        elif type(keySize) is int:
+            if keySize > 26 or keySize < 1:
+                print "enter a value between 1 and 26"
+                continue
+            else:
+                logic = crypt(en_de_crypt)
+                break
+
+        else:
+            print "Enter an integer key number (1-26)"
+            pass
+
+    if logic is True:
+        cipher = encryption(inputMessage, keySize)
+        print cipher
+    elif logic is False:
+        plain = decryption(inputMessage, keySize)
+        print plain
+    else:
+        pass
+
+
 def crypt(cryptInput):
-	#returns boolean value, true if encrypt, false if decrypt
-	
-	truth = False
-	
-	if (cryptInput == 'encrypt') or (cryptInput =='Encrypt'):
-	    truth = True
-	    return truth
-	elif (cryptInput == 'decrypt') or (cryptInput =='Decrypt'):
-	    truth = False
-	    return truth
-	else:
-		pass
+    # Returns boolean value, true if encrypt, false if decrypt.
+
+    truth = False
+
+    if (cryptInput == 'encrypt') or (cryptInput == 'Encrypt'):
+        truth = True
+        return truth
+    elif (cryptInput == 'decrypt') or (cryptInput == 'Decrypt'):
+        truth = False
+        return truth
+    else:
+        pass
+
 
 def encryption(plaintext, size):
-	#takes in message string and the key size, and runs caesar cipher
-		
-	cipher = list(plaintext) 
-	
-	for i in range(len(cipher)):
-		cipher[i] = ord(cipher[i])
-		
-		if cipher[i] is 32:
-		    cipher[i] = chr(cipher[i])
-		elif cipher[i] > 64 and cipher[i] < 91:
-		    temp = cipher[i] + size
-		    if temp >= 91:
-		        temp -= 90
-		        temp += 64
-		        cipher[i] = chr(temp)
-		    else:
-		        cipher[i] = chr(temp)    
-		    
-		elif cipher[i] > 96 and cipher[i] < 123:
-		    temp = cipher[i] + size
-		    if temp >= 123:
-		        temp -= 122
-		        temp += 96
-		        cipher[i] = chr(temp)
-		    else:
-		        cipher[i] = chr(temp)
-		 
-		else:
-		    cipher[i] = chr(cipher[i])
-	
-	output= ''.join(cipher)
-	
-	return output
-	
+    # Takes in message string and the key size, and runs caesar cipher
+
+    cipher = list(plaintext)
+
+    for i in range(len(cipher)):
+        cipher[i] = ord(cipher[i])
+
+        if cipher[i] is 32:
+            cipher[i] = chr(cipher[i])
+        elif cipher[i] > 64 and cipher[i] < 91:
+            temp = cipher[i] + size
+            if temp >= 91:
+                temp -= 90
+                temp += 64
+                cipher[i] = chr(temp)
+            else:
+                cipher[i] = chr(temp)
+
+        elif cipher[i] > 96 and cipher[i] < 123:
+            temp = cipher[i] + size
+            if temp >= 123:
+                temp -= 122
+                temp += 96
+                cipher[i] = chr(temp)
+            else:
+                cipher[i] = chr(temp)
+
+        else:
+            cipher[i] = chr(cipher[i])
+
+    output = ''.join(cipher)
+
+    return output
+
 
 def decryption(ciphertext, size):
-	#takes in the ciphertext and key size, performs reverse caeasar cipher
-	
-	plain = list(ciphertext) 
-	
-	for i in range(len(plain)):
-		plain[i] = ord(plain[i])
-		
-		if plain[i] is 32:
-		    plain[i] = chr(plain[i])
-		elif plain[i] > 64 and plain[i] < 91:
-		    temp = plain[i] - size
-		    if temp <= 64:
-		        temp += 26
-		        plain[i] = chr(temp)
-		    else:
-		        plain[i] = chr(temp)    
-		    
-		elif plain[i] > 96 and plain[i] < 123:
-		    temp = plain[i] - size
-		    if temp <= 96:
-		        temp += 26
-		        plain[i] = chr(temp)
-		    else:
-		        plain[i] = chr(temp)
-		 
-		else:
-		    plain[i] = chr(plain[i])
-	
-	output= ''.join(plain)
-	
-	return output
-		
-		
-if __name__ == '__main__': 
+    #Takes in the ciphertext and key size, performs reverse caeasar cipher
+
+    plain = list(ciphertext)
+
+    for i in range(len(plain)):
+        plain[i] = ord(plain[i])
+
+        if plain[i] is 32:
+            plain[i] = chr(plain[i])
+        elif plain[i] > 64 and plain[i] < 91:
+            temp = plain[i] - size
+            if temp <= 64:
+                temp += 26
+                plain[i] = chr(temp)
+            else:
+                plain[i] = chr(temp)
+
+        elif plain[i] > 96 and plain[i] < 123:
+            temp = plain[i] - size
+            if temp <= 96:
+                temp += 26
+                plain[i] = chr(temp)
+            else:
+                plain[i] = chr(temp)
+
+        else:
+            plain[i] = chr(plain[i])
+
+    output = ''.join(plain)
+
+    return output
+
+
+if __name__ == '__main__':
     main()
--- Enigma/enigma/tests/testMachine.py
+++ Enigma/enigma/tests/testMachine.py
@@ -1,25 +1,30 @@
 import unittest
 from enigma.machine import Rotor, Core
 
+
 class TestRotorMethods(unittest.TestCase):
     #Test for Rotor object
-    c1= ['A', 'B', 'C']
-    c2= ['D', 'E', 'F']
-    c3= ['G']
-      
-    
+    c1 = ['A', 'B', 'C']
+    c2 = ['D', 'E', 'F']
+    c3 = ['G']
+
     def test_Init(self):
         R1 = Rotor(self.c1)
         R2 = Rotor(self.c2)
         R3 = Rotor(self.c3)
 
         print R1.data
-        self.assertEqual(R1.data, self.c1, 'failed to install correct rotor config')
+        self.assertEqual(R1.data, self.c1,
+                         'failed to install correct rotor config'
+                         )
         print R2.data
-        self.assertEqual(R2.data, self.c2, 'failed to install correct rotor config')
+        self.assertEqual(R2.data, self.c2,
+                         'failed to install correct rotor config'
+                         )
         print R3.data
-        self.assertEqual(R3.data, self.c3, 'failed to install correct rotor config')
-
+        self.assertEqual(R3.data, self.c3,
+                         'failed to install correct rotor config'
+                         )
         print R1.current
         self.assertEqual(R1.current, self.c1[0])
         print R2.current
@@ -35,40 +40,41 @@ class TestRotorMethods(unittest.TestCase):
         R1.POSITION = 0
         R2.POSITION = 0
         R3.POSITION = 0
-         
+
         R1.rotate()
         self.assertEqual(R1.POSITION, 1)
-         
+
         for i in range(100):
             R2.rotate()
-            
+
         self.assertEqual(R2.POSITION, 1)
 
         R3.rotate()
         self.assertEqual(R3.POSITION, 0, 'It should rotate and remain 0')
-    
+
     def test_SetStart(self):
         R1 = Rotor(self.c1)
         R2 = Rotor(self.c2)
-        R3 = Rotor(self.c3) 
-         
+        R3 = Rotor(self.c3)
+
         P1 = 0
         P2 = 1
-        P3 = 35
+        P3 = 0
 
         R1.POSITION = P1
         R2.POSITION = P2
+        R3.POSITION = P3
 
         self.assertEqual(R1.POSITION, P1, 'incorrect position')
         self.assertEqual(R2.POSITION, P2, 'incorrect position')
 
+
 class TestCoreMethods(unittest.TestCase):
     #Test for Core object
 
-    c1= ['A', 'B', 'C']
-    c2= ['D', 'E', 'F']
-    c3= ['G']
-
+    c1 = ['A', 'B', 'C']
+    c2 = ['D', 'E', 'F']
+    c3 = ['G']
 
     def test_Init(self):
         R1 = Rotor(self.c1)
@@ -76,18 +82,18 @@ class TestCoreMethods(unittest.TestCase):
         R3 = Rotor(self.c3)
 
         coreImpl = Core(R1, R2, R3)
-        
+
         self.assertEqual(coreImpl.r1.data, self.c1)
         self.assertEqual(coreImpl.r2.data, self.c2)
         self.assertEqual(coreImpl.r3.data, self.c3)
-    
+
     def test_Config(self):
         R1 = Rotor(self.c1)
         R2 = Rotor(self.c2)
         R3 = Rotor(self.c3)
 
-        core = Core(R1, R2, R3) 
-        
+        core = Core(R1, R2, R3)
+
         core.config(R1, R2, R3, 2, 1, 0)
         print core.r1.current
         print core.r2.current
@@ -99,17 +105,18 @@ class TestCoreMethods(unittest.TestCase):
         self.assertEqual(core.r1.current, 'C', 'failed to assign first rotor')
         self.assertEqual(core.r2.current, 'E', 'failed to assign second rotor')
         self.assertEqual(core.r3.current, 'G', 'failed to assign third rotor')
-        
+
         core.config(R3, R1, R2, 0, 0, 0)
         self.assertEqual(core.r1.current, 'G', 'failed rotor reassignment')
         self.assertEqual(core.r2.current, 'A', 'failed rotor reassignment')
         self.assertEqual(core.r3.current, 'D', 'failed rotor reassignment')
-             
+
     def test_Encrypt(self):
-        x1 = ['A', 'B', 'C']  #changed to A-B-C bc hand tests are difficult to solve for with A-...-J
+    # Changed to A-B-C bc hand tests are difficult to solve for with A-...-J
+        x1 = ['A', 'B', 'C']
         x2 = ['A', 'B', 'C']
         x3 = ['A', 'B', 'C']
-        
+
         R1 = Rotor(x1)
         R2 = Rotor(x2)
         R3 = Rotor(x3)
@@ -122,13 +129,12 @@ class TestCoreMethods(unittest.TestCase):
         core.config(R1, R2, R3, 0, 0, 0)
         temp2 = core.encrypt('HELLO WORLD')
         self.assertEqual(temp2, 'HFNMQ YRVMF')
-        
-    
+
     def test_Decrypt(self):
         x1 = ['A', 'B', 'C']
         x2 = ['A', 'B', 'C']
         x3 = ['A', 'B', 'C']
-        
+
         R1 = Rotor(x1)
         R2 = Rotor(x2)
         R3 = Rotor(x3)
@@ -142,8 +148,6 @@ class TestCoreMethods(unittest.TestCase):
         core.config(R1, R2, R3, 0, 0, 0)
         temp2 = core.decrypt('HFNMQ YRVMF')
         self.assertEqual(temp2, 'HELLO WORLD')
-    
 
 if __name__ == "__main__":
     unittest.main()
-
